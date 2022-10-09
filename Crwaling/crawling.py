fc1d541f91ebc324c564a6b4d950284754696a3c
Todah01 <57752105+Todah01@users.noreply.github.com>
2022-10-09 19:21:49 +0900

    2022.10.09
    set crawling web

--- Crwaling/crawling.py
+++ Crwaling/crawling.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from html_table_parser import parser_functions as parser
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,6 +12,7 @@ import time
 import pandas as pd
 import numpy as np
 import requests
+import random
 
 
 def set_chrome_driver():
@@ -19,9 +21,20 @@ def set_chrome_driver():
     return web_driver
 
 
-url = "https://www.hjnc.co.kr/esvc/vessel/berthScheduleT"
+url = "https://swarfarm.com/bestiary/?page=1&sort=name%3Basc"
 
-# req = requests.get(url)
+dr = set_chrome_driver()
+dr.get(url)
+
+act = ActionChains(dr)
+
+columns_selectors = dr.find_elements(By.CLASS_NAME, 'form-check-input checked')
+
+print(columns_selectors)
+
+for selector in columns_selectors:
+    print(selector)
+
 # soup = BeautifulSoup(req.text, 'html.parser')
 # temp = soup.find('table', {'id': 'tblMaster'})
 #
@@ -30,3 +43,13 @@ url = "https://www.hjnc.co.kr/esvc/vessel/berthScheduleT"
 # options = webdriver.ChromeOptions()
 # options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
+# order_number_list = []
+# order_number = 0
+# idx = 0
+#
+# for i in range(4):
+#     while order_number in order_number_list or idx < 100:
+#         order_number = random.randint(0, 4)
+#         idx += 1
+#     order_number_list.append(order_number)
+    # print(order_number)
